7ecc0a659e8b0147349be7dce69eb98d9951c140
Makasya <maxytka228@gmail.com>
2025-09-08 19:10:26 +0500

    Task2

---
+++ Tyuiu.FisherMA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.FisherMA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.FisherMA.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Максим"));
+            Console.ReadKey();
+        }
+    }
+
+}

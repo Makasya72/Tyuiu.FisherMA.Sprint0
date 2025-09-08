2af10134941a5c23e31d417db4c69b06b72a8466
Makasya <maxytka228@gmail.com>
2025-09-08 19:25:25 +0500

    Task3

---
+++ Tyuiu.FisherMA.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.FisherMA.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.FisherMA.Sprint0.Task3.V0
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+    }
+}
